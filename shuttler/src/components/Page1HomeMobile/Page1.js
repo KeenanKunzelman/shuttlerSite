a2901988e5b7d8bbf143a4efb5282678b67aa4b8
Keith Prevost <rickyprev@yahoo.com>
2018-11-12 16:02:02 -0500

    home page below navBar

---
+++ shuttler/src/components/Page1HomeMobile/Page1.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import "./Page1.css";
+
+const homeBackground=(props)=>{
+
+return(
+<div class="container" >
+
+<div class="containsTitle">
+
+
+<h1><span class="title">Don't miss</span> the Shuttle ever again</h1>
+
+</div>
+
+<div className="mobileHomePage">
+
+
+</div>
+
+</div>
+)
+
+};
+
+export default homeBackground;
